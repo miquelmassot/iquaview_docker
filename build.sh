de96f2bbad3c47d5879f942e45ae28011b2612b3
Miquel Massot <miquel.massot@gmail.con>
2023-08-21 14:04:40 +0100

    Rename dockerfile

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t ghcr.io/ocean-perception/qgis:latest -f qgis.Dockerfile .
 docker build -t ghcr.io/ocean-perception/cola2_lib:latest -f cola2_lib.Dockerfile .
-docker build -t ghcr.io/ocean-perception/iquaview:latest .
+docker build -t ghcr.io/ocean-perception/iquaview:latest -f iquaview.Dockerfile .
